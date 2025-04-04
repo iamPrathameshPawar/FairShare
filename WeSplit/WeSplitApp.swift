4117993300f57a8b28e5099b8c8cfd4d4c3448c4
prathamesh.pawar <prathamesh.pawar@blancco.com>
2025-04-04 13:14:41 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Prathamesh Pawar on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
