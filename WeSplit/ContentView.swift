b1c797bd2931827a2d1b4c972af097c0a123d327
prathamesh.pawar <prathamesh.pawar@blancco.com>
2025-04-06 22:25:14 +0530

    Created the money split app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+        
+    var splitAmount: Double {
+        let peopleCount = Double(numberOfPeople)
+        let tipSelected = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelected
+        let grandTotal = checkAmount + tipValue
+        
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack {
+                
+                Color(.systemYellow)
+                    .ignoresSafeArea()
+                
+                Form{
+                    Section{
+                        TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                            .keyboardType(.decimalPad)
+                            .focused($amountIsFocused)
+                    }
+                    .listRowBackground(Color.cyan)
+                    
+                    Section("Split the check in") {
+                        Picker("Number of People", selection: $numberOfPeople) {
+                            ForEach(0..<101){
+                                Text("\($0) People")
+                            }
+                        }
+                    }
+                    
+                    Section{
+                        HStack{
+                            Text("Check Amount: ")
+                            Spacer()
+                            Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        }
+                    }
+                    
+                    Section("How much do you want to Tip?"){
+                        Picker("", selection: $tipPercentage){
+                            ForEach(tipPercentages, id: \.self){
+                                Text($0, format: .percent)
+                            }
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                    .listRowBackground(Color.green)
+                    
+                    Section("Calculation: "){
+                        Text("Check Amount = \(checkAmount)")
+                        Text("After \(tipPercentage) % of tip = \((checkAmount / 100 * Double(tipPercentage)) + checkAmount)")
+                        Text("Total amount divided into \(numberOfPeople) People = \(splitAmount)")
+                    }
+                    
+                    Section{
+                        HStack{
+                            Text("Split Amount: ")
+                            Spacer()
+                            Text(splitAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        }
+                        .listRowBackground(Color.mint)
+                    }
+                }
+                .scrollContentBackground(.hidden)
+            }
+            .navigationTitle("FairShare")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
